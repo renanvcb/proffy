72fa46374844524afd84f9fc070d0cceef60a0ac
renanvcb <renanvcb@gmail.com>
2020-08-04 03:41:40 -0300

    First commit

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/38444942?s=460&u=786fddc78b1b3d0904dda49588b20107ebb32e82&v=4" alt="Renan Borges" />
+        <div>
+          <strong>Renan Borges</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+            <br /><br />
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+          </p>
+
+      <footer>
+        <p>
+          Preço/Hora
+              <strong>R$ 20,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+              Entrar em contato
+            </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
